aa645c39f3d4b25769d0550e32639d07edfff2b0
Jakub Golonka <jakubgolonka216@gmail.com>
2025-05-29 08:55:45 +0200

    Update HarmonogramPage.xaml.cs

--- KalkulatorRatalnyMAUI/HarmonogramPage.xaml.cs
+++ KalkulatorRatalnyMAUI/HarmonogramPage.xaml.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace KalkulatorRatalnyMAUI
 {
-    // Strona wyœwietlaj¹ca harmonogram sp³at kredytu i umo¿liwiaj¹ca zapis do PDF
+    // Strona wyÅ“wietlajÂ¹ca harmonogram spÂ³at kredytu i umoÂ¿liwiajÂ¹ca zapis do PDF
     public partial class HarmonogramPage : ContentPage
     {
         // Lista pozycji harmonogramu przekazywana z poprzedniej strony
@@ -23,32 +23,36 @@ namespace KalkulatorRatalnyMAUI
             HarmonogramListView.ItemsSource = harmonogram;
         }
 
-        // Obs³uga powrotu do poprzedniej strony
+        // ObsÂ³uga powrotu do poprzedniej strony
         private async void OnWrocClicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
 
-        // Obs³uga zapisu harmonogramu do pliku PDF (wywo³uje metodê generuj¹c¹ PDF asynchronicznie)
+        // ObsÅ‚uga zapisu harmonogramu do pliku PDF (wywoÅ‚uje metodÄ™ generujÄ…cÄ… PDF asynchronicznie)
         private async void OnSavePdfClicked(object sender, EventArgs e)
         {
-            try
-            {
-                var filePath = await GeneratePdfAsync(harmonogram);
-                await DisplayAlert("Sukces", $"Plik PDF zosta³ zapisany:\n{filePath}", "OK");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("B³¹d", $"Nie uda³o siê wygenerowaæ PDF:\n{ex.Message}", "OK");
-            }
+            #if WINDOWS
+                try
+                {
+                    var filePath = await GeneratePdfAsync(harmonogram);
+                    await DisplayAlert("Sukces", $"Plik PDF zostaÅ‚ zapisany:\n{filePath}", "OK");
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("BÅ‚Ä…d", $"Nie udaÅ‚o siÄ™ wygenerowaÄ‡ PDF:\n{ex.Message}", "OK");
+                }
+            #else
+                await DisplayAlert("Informacja", "Generowanie PDF jest obecnie obsÅ‚ugiwane tylko w systemie Windows.", "OK");
+            #endif
         }
 
-        // Kluczowa metoda generuj¹ca PDF z harmonogramem
+        // Kluczowa metoda generujÂ¹ca PDF z harmonogramem
         private static Task<string> GeneratePdfAsync(List<HarmonogramItem> data)
         {
             return Task.Run(() =>
             {
-                // Przygotowanie pliku PDF z nazw¹ zawieraj¹c¹ datê i zapis w folderze Dokumenty
+                // Przygotowanie pliku PDF z nazwÂ¹ zawierajÂ¹cÂ¹ datÃª i zapis w folderze Dokumenty
                 string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 string filename = $"Harmonogram_{DateTime.Now:yyyyMMdd_HHmmss}.pdf";
                 string fullPath = Path.Combine(documentsPath, filename);
@@ -60,35 +64,35 @@ namespace KalkulatorRatalnyMAUI
                 // Obiekt do rysowania grafiki na stronie PDF
                 XGraphics gfx = XGraphics.FromPdfPage(page);
 
-                // Czcionki do ró¿nych elementów PDF
+                // Czcionki do rÃ³Â¿nych elementÃ³w PDF
                 var fontTitleBig = new XFont("Arial", 20, XFontStyle.Bold);
                 var fontTitleSmall = new XFont("Arial", 14, XFontStyle.Regular);
                 var fontRegular = new XFont("Arial", 12, XFontStyle.Regular);
                 var fontBold = new XFont("Arial", 12, XFontStyle.Bold);
 
-                // Marginesy i szerokoœæ do rysowania tabeli
+                // Marginesy i szerokoÅ“Ã¦ do rysowania tabeli
                 double marginLeft = 40;
                 double marginRight = 40;
                 double usableWidth = page.Width - marginLeft - marginRight;
 
                 double yPoint = 40; // pozycja pionowa startowa
 
-                // Rysujemy tytu³ i podtytu³y (m.in. aktualn¹ datê)
+                // Rysujemy tytuÂ³ i podtytuÂ³y (m.in. aktualnÂ¹ datÃª)
                 gfx.DrawString("Kalkulator Ratalny", fontTitleBig, XBrushes.Black,
                     new XRect(marginLeft, yPoint, usableWidth, 25), XStringFormats.Center);
                 yPoint += 40;
-                gfx.DrawString("Harmonogram sp³at kredytu", fontTitleSmall, XBrushes.Black,
+                gfx.DrawString("Harmonogram spÂ³at kredytu", fontTitleSmall, XBrushes.Black,
                     new XRect(marginLeft, yPoint, usableWidth / 2, 25), XStringFormats.TopLeft);
                 gfx.DrawString(DateTime.Now.ToString("dd.MM.yyyy HH:mm"), fontTitleSmall, XBrushes.Black,
                     new XRect(marginLeft + usableWidth / 2, yPoint, usableWidth / 2, 25), XStringFormats.TopRight);
                 yPoint += 40;
 
-                // Nag³ówki tabeli i szerokoœci kolumn (5 kolumn)
+                // NagÂ³Ã³wki tabeli i szerokoÅ“ci kolumn (5 kolumn)
                 int columnsCount = 5;
                 double columnWidth = usableWidth / columnsCount;
-                var headers = new[] { "Numer", "Kwota raty", "Kapita³", "Odsetki", "Pozosta³e saldo" };
+                var headers = new[] { "Numer", "Kwota raty", "KapitaÂ³", "Odsetki", "PozostaÂ³e saldo" };
 
-                // Rysujemy nag³ówki z szarym t³em
+                // Rysujemy nagÂ³Ã³wki z szarym tÂ³em
                 double xPos = marginLeft;
                 var headerBackground = new XSolidBrush(XColor.FromArgb(220, 220, 220));
 
@@ -110,7 +114,7 @@ namespace KalkulatorRatalnyMAUI
                 {
                     xPos = marginLeft;
 
-                    // W ka¿dej kolumnie rysujemy odpowiedni¹ wartoœæ
+                    // W kaÂ¿dej kolumnie rysujemy odpowiedniÂ¹ wartoÅ“Ã¦
                     void DrawCell(string text)
                     {
                         var rect = new XRect(xPos, yPoint, columnWidth, 25);
@@ -129,7 +133,7 @@ namespace KalkulatorRatalnyMAUI
 
                     yPoint += 25;
 
-                    // Jeœli zabraknie miejsca na stronie, dodajemy now¹ stronê i powtarzamy nag³ówki
+                    // JeÅ“li zabraknie miejsca na stronie, dodajemy nowÂ¹ stronÃª i powtarzamy nagÂ³Ã³wki
                     if (yPoint + 25 > page.Height - 40)
                     {
                         gfx.Dispose();
@@ -140,11 +144,11 @@ namespace KalkulatorRatalnyMAUI
 
                         yPoint = 40;
 
-                        // Rysujemy tytu³ i nag³ówki ponownie na nowej stronie
+                        // Rysujemy tytuÂ³ i nagÂ³Ã³wki ponownie na nowej stronie
                         gfx.DrawString("Kalkulator Ratalny", fontTitleBig, XBrushes.Black,
                             new XRect(marginLeft, yPoint, usableWidth, 25), XStringFormats.Center);
                         yPoint += 40;
-                        gfx.DrawString("Harmonogram sp³at kredytu", fontTitleSmall, XBrushes.Black,
+                        gfx.DrawString("Harmonogram spÂ³at kredytu", fontTitleSmall, XBrushes.Black,
                             new XRect(marginLeft, yPoint, usableWidth / 2, 25), XStringFormats.TopLeft);
                         gfx.DrawString(DateTime.Now.ToString("dd.MM.yyyy HH:mm"), fontTitleSmall, XBrushes.Black,
                             new XRect(marginLeft + usableWidth / 2, yPoint, usableWidth / 2, 25), XStringFormats.TopRight);
@@ -169,7 +173,7 @@ namespace KalkulatorRatalnyMAUI
 
                 gfx.Dispose();
 
-                // Zapisujemy plik PDF do podanej lokalizacji i zwracamy tê œcie¿kê
+                // Zapisujemy plik PDF do podanej lokalizacji i zwracamy tÃª Å“cieÂ¿kÃª
                 document.Save(fullPath);
                 return fullPath;
             });
